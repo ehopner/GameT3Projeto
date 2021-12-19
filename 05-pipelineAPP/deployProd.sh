5c522d05c920f66c10d11fa4fb42387ea0adefef
Eduardo Hopner <ehpemc@gmail.com>
2021-12-19 20:21:30 +0000

    alterado mas nao funcional

--- 05-pipelineAPP/deployProd.sh
+++ 05-pipelineAPP/deployProd.sh
@@ -5,5 +5,5 @@ cd 05-pipelineAPP/ansible
 
 #ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/chaveprivada.pem
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarProd.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key $TF_VAR_PATH_KEY ]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarProd.yml -u ubuntu --private-key $TF_VAR_PATH_KEY
--- 05-pipelineAPP/deployStag.sh
+++ 05-pipelineAPP/deployStag.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+export TF_VAR_PATH_KEY="/home/ubuntu/ehopner-dev.pem"
 
 cd 05-pipelineAPP/ansible
 
 #ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ~/.ssh/chaveprivada.pem
 
-echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa ]"
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarStag.yml -u ubuntu --private-key /var/lib/jenkins/.ssh/id_rsa
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key $TF_VAR_PATH_KEY ]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionarStag.yml -u ubuntu --private-key $TF_VAR_PATH_KEY
